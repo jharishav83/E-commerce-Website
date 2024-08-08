9665194fa1bc369a2d4b876256f0fd694cf77ebd
jharishav83 <rishavjha0697@gmail.com>
2024-08-08 19:16:35 +0530

    My first Commit E-commerce -website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
-
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
+import Error from "./Error";
+import { GlobalStyle } from "./GlobalStyle";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Router>
+    <GlobalStyle/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/products" element={<Products/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/singleproduct/:id" element={<SingleProduct/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="*" element={<Error/>}/>
+
+    </Routes>
+  </Router>
+  </>
   );
 }
 
